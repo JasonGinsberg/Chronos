a6358472daacf129400df15548fc3ac98d222c82
Jason Ginbserg <Jason@new-host-4.home>
2013-07-08 08:18:53 -0400

    Initial Commit

---
+++ past time/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  past time
+//
+//  Created by Jason Ginsberg on 7/8/13.
+//  Copyright (c) 2013 Jason Ginsberg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ past timeTests/past_timeTests.m
@@ -0,0 +1,36 @@
+//
+//  past_timeTests.m
+//  past timeTests
+//
+//  Created by Jason Ginsberg on 7/8/13.
+//  Copyright (c) 2013 Jason Ginsberg. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface past_timeTests : XCTestCase
+
+@end
+
+@implementation past_timeTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
